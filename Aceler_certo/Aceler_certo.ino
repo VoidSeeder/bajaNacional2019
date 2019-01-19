33a1efc369d2ac6e2dd2cbd8ab9d3cda90578beb
VoidSeeder <jpvf482@gmail.com>
2019-01-19 09:51:05 -0300

    RTC e acelerometro corrigidos

--- Aceler_certo/Aceler_certo.ino
+++ Aceler_certo/Aceler_certo.ino
@@ -2,11 +2,16 @@
 //Alteracoes e adaptacoes : Arduino e Cia
 //
 //Baseado no programa original de JohnChi.
- 
+
 //Carrega a biblioteca Wire
 #include <Wire.h>
 
-#include <DS3232RTC.h>      // https://github.com/JChristensen/DS3232RTC
+// Carrega a biblioteca virtuabotixRTC
+#include <virtuabotixRTC.h>           
+
+// Determina os pinos ligados ao modulo
+// myRTC(clock, data, rst)
+virtuabotixRTC myRTC(7, 6, 5);
 
 //Endereco I2C do MPU6050
 const int MPU=0x68;  
@@ -25,7 +30,10 @@ void setup()
   Wire.write(0); 
   Wire.endTransmission(true);
 
-  setSyncProvider(RTC.get);
+  // Informacoes iniciais de data e hora
+  // Apos setar as informacoes, comente a linha abaixo
+  // (segundos, minutos, hora, dia da semana, dia do mes, mes, ano)
+  //myRTC.setDS1302Time(00, 30, 9, 7, 19, 1, 2019);
 }
 
 void loop()
@@ -55,13 +63,71 @@ void loop()
   Serial.print(" | Y = "); Serial.print(GyY);
   Serial.print(" | Z = "); Serial.print(GyZ);
   Serial.print(" | Temp = "); Serial.println(Tmp/340.00+36.53);
-  Serial.print(hour()); Serial.print(":");
-  Serial.print(minute()); Serial.print(":");
-  Serial.print(second()); Serial.print(" - ");
-  Serial.print(day()); Serial.print("/");
-  Serial.print(month()); Serial.print("/");
-  Serial.println(year());
+
+  // Le as informacoes do CI
+  myRTC.updateTime(); 
+
+  // Imprime as informacoes no serial monitor
+  Serial.print("Data : ");
+  // Chama a rotina que imprime o dia da semana
+  imprime_dia_da_semana(myRTC.dayofweek);
+  Serial.print(", ");
+  Serial.print(myRTC.dayofmonth);
+  Serial.print("/");
+  Serial.print(myRTC.month);
+  Serial.print("/");
+  Serial.print(myRTC.year);
+  Serial.print("  ");
+  Serial.print("Hora : ");
+  // Adiciona um 0 caso o valor da hora seja <10
+  if (myRTC.hours < 10)
+  {
+    Serial.print("0");
+  }
+  Serial.print(myRTC.hours);
+  Serial.print(":");
+  // Adiciona um 0 caso o valor dos minutos seja <10
+  if (myRTC.minutes < 10)
+  {
+    Serial.print("0");
+  }
+  Serial.print(myRTC.minutes);
+  Serial.print(":");
+  // Adiciona um 0 caso o valor dos segundos seja <10
+  if (myRTC.seconds < 10)
+  {
+    Serial.print("0");
+  }
+  Serial.println(myRTC.seconds);
     
   //Aguarda 300 ms e reinicia o processo
   delay(300);
+}
+
+void imprime_dia_da_semana(int dia)
+{
+  switch (dia)
+  {
+    case 1:
+    Serial.print("Domingo");
+    break;
+    case 2:
+    Serial.print("Segunda");
+    break;
+    case 3:
+    Serial.print("Terca");
+    break;
+    case 4:
+    Serial.print("Quarta");
+    break;
+    case 5:
+    Serial.print("Quinta");
+    break;
+    case 6:
+    Serial.print("Sexta");
+    break;
+    case 7:
+    Serial.print("Sabado");
+    break;
+  }
 }
